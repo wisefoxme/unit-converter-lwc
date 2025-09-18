cbfdc9301ae9310cb49013dece9cb88bb0592231
Renato Oliveira <renato.oliveira@dogeforce.com>
2025-09-18 00:20:38 -0300

    chore(docs): updates sample

--- force-app/sample/default/lwc/sample/sample.js
+++ force-app/sample/default/lwc/sample/sample.js
@@ -1,3 +1,12 @@
 import { LightningElement } from "lwc";
 
-export default class Sample extends LightningElement {}
+export default class Sample extends LightningElement {
+  lastChange = "";
+
+  handleChange(event) {
+    const from = event.detail.from;
+    const to = event.detail.to;
+
+    this.lastChange = `Value changed from ${from} to ${to}`;
+  }
+}
